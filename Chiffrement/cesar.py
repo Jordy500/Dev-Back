2d5aa05ca759888704f4e2c15ef39323eb61084e
Jordy500 <156438783+Jordy500@users.noreply.github.com>
2024-12-17 09:03:15 +0100

    Caesar cipher with brute force

--- Chiffrement/cesar.py
+++ Chiffrement/cesar.py
@@ -1,7 +1,55 @@
 import string
 
-def cesar(texte, decalage):
-    alphabet = string.ascii_lowercase
+def cesar_cipher(message, key):
+    return cesar(message, key)
 
-    texte = []
+def cesar(message, key):
     
+    crypted_message = ""
+    
+    for char in message :
+
+        index_carac_in_printable = alphabet.index(char)
+        index_crypted_char = (index_carac_in_printable + key) % len(alphabet)
+        crypted_char = alphabet[index_crypted_char]
+
+        crypted_message += crypted_char
+    
+    return crypted_message
+
+# Dechiffrement Proposé Prof
+'''
+def dechiffrer_cesar(message, key):
+    
+    return cesar (message, -key)
+
+# Dechiffremet Perso
+
+''.join([alphabet[(alphabet.index(carac) - key) % len(alphabet)] 
+                    if carac in alphabet else carac for carac in message])
+
+
+print(dechiffrer_cesar("coucou", 520))
+'''
+# Brute Force
+'''
+def bruteforce_cipher(message):
+    return cesar(message, min(range(26), key=lambda x: sum(c1 != c2 for c1, c2 in zip(message, cesar(message, x)))))
+'''
+def cesar_uncipher(crypted_message, key):
+	return cesar_cipher(crypted_message, -key)
+
+crypted_message = cesar("salut les bgs aujourd'hui on va corriger l'exercice", 13)
+print(crypted_message)
+
+def brute_force_cesar_cipher(crypted_message):
+	for possible_key in range(0, len(alphabet)):
+            
+		print(cesar_uncipher(crypted_message, possible_key))
+            
+		print("_"*15)
+
+
+brute_force_cesar_cipher(crypted_message)
+
+print(cesar("coucou", 520))
--- Chiffrement/vigenere.py
+++ Chiffrement/vigenere.py
@@ -1,3 +1,24 @@
-list_numbers = [7, 8, 9, 28, 33,]
+import string
 
-print(list_numbers[4])
+def chiffre_vigenere(message, key):
+    alphabet = string.ascii_lowercase + "éèêàôëùïü"
+    vigenere_key = [ord(k) - ord('a') for k in key.lower()]
+    
+    vigenere_message = []
+    
+    key_index = 0
+    
+    for char in message:
+
+            shift = vigenere_key[key_index % len(vigenere_key)]
+            char_index = alphabet.index(char)
+            new_index = (char_index + shift) % len(alphabet)
+            vigenere_message.append(alphabet[new_index])
+            key_index += 1
+
+    
+    return ''.join(vigenere_message)
+
+message = "je me debrouillé"
+key = "abc"
+print(chiffre_vigenere(message, key))
